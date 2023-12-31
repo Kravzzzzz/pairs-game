790e392c40925f60681a0b0c08fedad55a61cc62
Pavel Krautsou <101918585+Kravzzzzz@users.noreply.github.com>
2023-12-31 14:55:54 +0300

    fix grid, add images

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,46 @@
-const field = document.createElement("div");
-document.body.appendChild(field);
-field.classList.add("field");
-
-let numberOfPairs = 2;
-const cardsArray = [];
-let firstOnenedCard = 0;
-let secondOpenedCard = 0;
-let tempOne;
-let tempTwo;
-let counter = 0;
-
-for (let i = 1; i <= numberOfPairs; i++) {
-  cardsArray.push(i, i);
-}
-
-//mix number in array
-for (let i = 0; i < cardsArray.length; i++) {
-  let randomNumber = Math.floor(Math.random() * cardsArray.length);
-  let temp = cardsArray[i];
-  cardsArray[i] = cardsArray[randomNumber];
-  cardsArray[randomNumber] = temp;
-}
-
-for (let i = 0; i < cardsArray.length; i++) {
-  let card = document.createElement("div");
-  field.appendChild(card);
-  card.classList.add("card");
-  card.setAttribute("pictureNumber", cardsArray[i]);
-}
-
-const cards = document.getElementsByClassName("card");
-
 function game() {
+  const field = document.createElement("div");
+  document.body.appendChild(field);
+  field.classList.add("field");
+
+  let numberOfPairs = prompt("enter the number of pairs of cards", 6);
+  const cardsArray = [];
+  let firstOnenedCard = 0;
+  let secondOpenedCard = 0;
+  let tempOne;
+  let tempTwo;
+  let counter = 0;
+  let colums = 2;
+  if(numberOfPairs >= 16){colums = 8}
+  else if (numberOfPairs % 5 == 0){colums = 5}
+  else if (numberOfPairs >= 9){colums = 6}
+  else if (numberOfPairs >= 4){colums = 4}
+  else if (numberOfPairs == 3){colums = 3}
+
+
+  field.style=`grid-template-columns: repeat(${colums}, 1fr)`
+
+  for (let i = 1; i <= numberOfPairs; i++) {
+    cardsArray.push(i, i);
+  }
+
+  //mix number in array
+  for (let i = 0; i < cardsArray.length; i++) {
+    let randomNumber = Math.floor(Math.random() * cardsArray.length);
+    let temp = cardsArray[i];
+    cardsArray[i] = cardsArray[randomNumber];
+    cardsArray[randomNumber] = temp;
+  }
+
+  for (let i = 0; i < cardsArray.length; i++) {
+    let card = document.createElement("div");
+    field.appendChild(card);
+    card.classList.add("card");
+    card.setAttribute("pictureNumber", cardsArray[i]);
+  }
+
+  const cards = document.getElementsByClassName("card");
+
   for (let card of cards) {
     card.addEventListener("click", () => {
       if (!firstOnenedCard) {
@@ -48,6 +57,13 @@ function game() {
           firstOnenedCard = 0;
           secondOpenedCard = 0;
           counter += 2;
+          if (counter === cards.length) {
+            setTimeout(() => {
+              alert("you win!");
+              field.innerHTML = "";
+              game();
+            }, 300);
+          }
         }
       } else {
         tempOne.style.pointerEvents = "auto";
@@ -57,13 +73,7 @@ function game() {
         secondOpenedCard = 0;
       }
     });
-    console.log(cards.length);
-    console.log(counter);
   }
-  // if(counter = cards.length){
-  //   setTimeout(() => {alert('you win!')}, 300)
-  // }
-
 }
 
 game();
